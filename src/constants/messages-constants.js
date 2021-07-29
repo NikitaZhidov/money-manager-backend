3bc865cdae9ea2c9baa8238454652756afb74cb3
Zhidov <zhidov@gkh365.ru>
2021-07-29 22:04:16 +0300

    init messages constants

---
+++ src/constants/messages-constants.js
@@ -0,0 +1,18 @@
+const ERR_MSG = {
+	WRONG_AUTH_INFO: 'Неверный email или пароль',
+	UNKNOWN: 'Непредвиденная ошибка',
+	EMAIL_EXISTS: 'Данный email уже зарегистрирован',
+	INVALID_ACTIVATED_LINK: 'Некорректная ссылка для активации аккаунта',
+	UNAUTHORIZED: 'Вы не авторизованы',
+	AUTH_ERROR: 'Произошла ошибка при авторизации',
+	SESSION_EXPIRES: 'Ваша сессия истекла',
+};
+
+const MSG = {
+	OK: 'OK',
+};
+
+module.exports = {
+	ERR_MSG,
+	MSG,
+};
